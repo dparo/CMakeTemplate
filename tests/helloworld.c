9e8df5fbe1f30c34cf0a62c196602e9f3d38e3d6
Davide Paro <dparo@outlook.it>
2021-05-25 11:31:48 +0200

    Some more example tests.

--- tests/helloworld.c
+++ tests/helloworld.c
@@ -8,9 +8,19 @@ static void test_example(void) {
     TEST_ASSERT_EQUAL(1 + 2, 3);
 }
 
+static void test_read(void)
+{
+    FILE *f = fopen("./CMakeLists.txt", "r");
+    char buffer[16 * 1024];
+    size_t sizeRead = fread(buffer, 1, sizeof(buffer), f);
+    TEST_ASSERT_GREATER_OR_EQUAL(64, sizeRead);
+    fclose(f);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_example);
+    RUN_TEST(test_read);
     return UNITY_END();
 }
 
