a3ac3391159a694f6554c947473cc8dca63cad5c
Davide Paro <dparo@outlook.it>
2023-05-15 08:02:22 +0200

    Switched to ThrowTheSwitch/Unity

--- tests/simple.c
+++ tests/simple.c
@@ -7,60 +7,60 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <greatest.h>
+#include <unity.h>
 
 #include "sum.h"
 #include <string.h>
 
-TEST example(void) {
-    ASSERT_EQ(1 + 2, 3);
-    PASS();
+void setUp(void)
+{
+  /* This is run before EACH TEST */
 }
 
-TEST calling_fread(void) {
+void tearDown(void)
+{
+}
+
+
+static void test_example(void) {
+    TEST_ASSERT_EQUAL(3, 1 + 2);
+}
+
+static void test_calling_fread(void) {
     FILE *f = fopen("./CMakeLists.txt", "r");
     char buffer[16 * 1024];
     size_t sizeRead = fread(buffer, 1, sizeof(buffer), f);
-    ASSERT_GTE(sizeRead, 64);
     fclose(f);
-    PASS();
+    TEST_ASSERT_GREATER_OR_EQUAL(64, sizeRead);
 }
 
-TEST calling_calloc_0_0(void) {
+static void test_calling_calloc_0_0(void) {
     char *p = calloc(0, 0);
-    ASSERT(p);
+    TEST_ASSERT_NOT_NULL(p);
     free(p);
-    PASS();
 }
 
-TEST calling_malloc_0(void) {
+static void test_calling_malloc_0(void) {
     char *p = malloc(0);
-    ASSERT(p);
+    TEST_ASSERT_NOT_NULL(p);
     free(p);
-    PASS();
 }
 
-TEST calling_sum(void) {
+static void test_calling_sum(void) {
     for (int32_t i = -10; i < 10; i++) {
         for (int32_t j = -10; j < 10; j++) {
-            ASSERT_EQ(i + j, sum(i, j));
+            TEST_ASSERT_EQUAL(i + j, sum(i, j));
         }
     }
-    PASS();
 }
 
-/* Add all the definitions that need to be in the test runner's main file. */
-GREATEST_MAIN_DEFS();
-
-int main(int argc, char **argv) {
-    GREATEST_MAIN_BEGIN(); /* command-line arguments, initialization. */
-
-    /* If tests are run outside of a suite, a default suite is used. */
-    RUN_TEST(example);
-    RUN_TEST(calling_fread);
-    RUN_TEST(calling_calloc_0_0);
-    RUN_TEST(calling_malloc_0);
-    RUN_TEST(calling_sum);
-
-    GREATEST_MAIN_END(); /* display results */
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_example);
+    RUN_TEST(test_calling_fread);
+    RUN_TEST(test_calling_calloc_0_0);
+    RUN_TEST(test_calling_malloc_0);
+    RUN_TEST(test_calling_sum);;
+    return UNITY_END();
 }
