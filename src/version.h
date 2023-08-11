e78e22eb22d364fb76201e4d187af68a298db067
Davide Paro <dparo@outlook.it>
2023-08-11 11:20:19 +0200

    General format fixes

--- src/version.h
+++ src/version.h
@@ -8,20 +8,20 @@
 extern "C" {
 #endif
 
-extern const char *PROJECT_NAME;
-extern const char *PROJECT_DESCRIPTION;
-extern const char *PROJECT_VERSION;
-extern const char *PROJECT_VERSION_MAJOR;
-extern const char *PROJECT_VERSION_MINOR;
-extern const char *PROJECT_VERSION_PATCH;
+extern const char *const PROJECT_NAME;
+extern const char *const PROJECT_DESCRIPTION;
+extern const char *const PROJECT_VERSION;
+extern const char *const PROJECT_VERSION_MAJOR;
+extern const char *const PROJECT_VERSION_MINOR;
+extern const char *const PROJECT_VERSION_PATCH;
 
-extern const char *BUILD_TYPE;
-extern const char *C_COMPILER_ID;
-extern const char *C_COMPILER_ABI;
-extern const char *C_COMPILER_VERSION;
-extern const char *GIT_SHA1;
-extern const char *GIT_DATE;
-extern const char *GIT_COMMIT_SUBJECT;
+extern const char *const BUILD_TYPE;
+extern const char *const C_COMPILER_ID;
+extern const char *const C_COMPILER_ABI;
+extern const char *const C_COMPILER_VERSION;
+extern const char *const GIT_SHA1;
+extern const char *const GIT_DATE;
+extern const char *const GIT_COMMIT_SUBJECT;
 
 #if __cplusplus
 }
